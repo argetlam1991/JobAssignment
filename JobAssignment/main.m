ced68d244ec877a853607ec92d7ebbfe2db08082
Gu Han <guhan@Gus-MacBook-Pro.local>
2017-06-10 19:17:48 -0700

    Initial Commit

---
+++ JobAssignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JobAssignment
+//
+//  Created by Gu Han on 6/10/17.
+//  Copyright © 2017 Gu Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
